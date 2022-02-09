0fa69f318f1407d394165253f9eabbbd87aedb08
Hasu <hshue83@gmail.com>
2022-02-09 16:43:40 +0900

    feat: rewrite with class

--- denops/Defie/actions.ts
+++ denops/Defie/actions.ts
@@ -1,6 +1,4 @@
-import { batch, buffers, Denops, globals } from "./deps.ts";
-import { DefieUtil } from "./util.ts";
-import { fn } from "./deps.ts";
+import { batch, Denops, fn } from "./deps.ts";
 
 export class DefieActions {
   basePath = "";
@@ -9,17 +7,31 @@ export class DefieActions {
   // Actions
   //
   async start(denops: Denops, path: string): Promise<void> {
-    this.basePath = await denops.call("expand", path);
+    this.basePath = (await denops.call("expand", path)) + "/";
 
     await this.walk(this.basePath).then((files) => {
       this.bufInit(denops, files);
     });
   }
+  async open(denops: Denops, direct: string): Promise<void> {
+    let path = await this.fullPath(denops, await denops.call("getline", "."));
+    let cmd: string = "edit";
+
+    if (path.endsWith("/")) cmd = "Defie";
+    if (direct === "tab") cmd = "tabedit";
+    if (direct === "vsplit") cmd = "vnew";
+
+    this.vimFeedKeys(denops, cmd, path.replace(/\/$/, ""));
+    await denops.cmd("setlocal modifiable buftype= ");
+  }
+
+  //
+  //Utils
+  //
   async bufInit(denops: Denops, files: Array<string>): Promise<void> {
     const bufnr = await fn.bufadd(denops, "Defie");
     await batch(denops, async (denops: Denops) => {
       await denops.cmd(`buffer ${bufnr}`);
-      await buffers.set(denops, "base_path", this.basePath + "/");
       await denops.cmd(
         "setlocal filetype=defie buftype=nofile modifiable nobuflisted"
       );
@@ -61,54 +73,46 @@ export class DefieActions {
     });
     return array;
   }
-}
+  async fullPath(denops: Denops, filename: string): Promise<string> {
+    return await denops.call(
+      "fnamemodify",
+      `${this.basePath}${filename}`,
+      ":p"
+    );
+  }
 
-export async function start(denops: Denops, path: string): Promise<void> {
-  const util = new DefieUtil(denops);
-
-  path = await denops.call("expand", path);
-
-  let files: Array<string> = [];
-
-  await util.entriesGetter(path, files);
-  await bufInit(denops, path, files);
+  async vimFeedKeys(denops: Denops, cmd: string, arg: string): Promise<void> {
+    await denops.cmd(`${cmd} ${arg}`);
+  }
 }
 
 //Open file or sub directory
-export async function defieOpen(denops: Denops, direct: string): Promise<void> {
-  let path = await makeFullPath(denops, await denops.call("getline", "."));
-  let cmd: string = "edit";
-
-  if (path.endsWith("/")) cmd = "Defie";
-  if (direct === "tab") cmd = "tabedit";
-  if (direct === "vsplit") cmd = "vnew";
-
-  callVimFeedKeys(denops, cmd, path.replace(/\/$/, ""));
-  await denops.cmd("setlocal modifiable buftype= ");
-}
-
-//Move parent directory
-export async function defie_up(denops: Denops): Promise<void> {
-  let path = (await buffers.get(denops, "base_path")) as string;
-
-  path = await denops.call(
-    "fnamemodify",
-    path.replace(/\/$/, ""),
-    ":p:h:h:gs!\\!/!"
-  );
-
-  callVimFeedKeys(denops, "Defie", path);
-}
-
-async function makeFullPath(denops: Denops, filename: string): Promise<string> {
-  const base = (await buffers.get(denops, "base_path")) as string;
-  return await denops.call("fnamemodify", `${base}${filename}`, ":p");
-}
-
-async function callVimFeedKeys(
-  denops: Denops,
-  cmd: string,
-  arg: string
-): Promise<void> {
-  await denops.cmd(`${cmd} ${arg}`);
-}
+//export async function defieOpen(denops: Denops, direct: string): Promise<void> {
+//  let path = await fullPath(denops, await denops.call("getline", "."));
+//  let cmd: string = "edit";
+//
+//  if (path.endsWith("/")) cmd = "Defie";
+//  if (direct === "tab") cmd = "tabedit";
+//  if (direct === "vsplit") cmd = "vnew";
+//
+//  vimFeedKeys(denops, cmd, path.replace(/\/$/, ""));
+//  await denops.cmd("setlocal modifiable buftype= ");
+//}
+//
+////Move parent directory
+//export async function defie_up(denops: Denops): Promise<void> {
+//  let path = (await buffers.get(denops, "base_path")) as string;
+//
+//  path = await denops.call(
+//    "fnamemodify",
+//    path.replace(/\/$/, ""),
+//    ":p:h:h:gs!\\!/!"
+//  );
+//
+//  vimFeedKeys(denops, "Defie", path);
+//}
+//
+//async function fullPath(denops: Denops, filename: string): Promise<string> {
+//  const base = (await buffers.get(denops, "base_path")) as string;
+//  return await denops.call("fnamemodify", `${base}${filename}`, ":p");
+//}
--- denops/Defie/main.ts
+++ denops/Defie/main.ts
@@ -1,5 +1,5 @@
 import { Denops } from "./deps.ts";
-import { defie_up, defieOpen, start } from "./actions.ts";
+//import { defie_up, defieOpen, start } from "./actions.ts";
 import { DefieActions } from "./actions.ts";
 import { ArgType } from "./types.ts";
 
@@ -11,10 +11,10 @@ export async function main(denops: Denops): Promise<void> {
 
       switch (validated_args.action) {
         case "open":
-          defieOpen(denops, validated_args.direction);
+          actions.open(denops, validated_args.direction);
           break;
         case "up":
-          defie_up(denops);
+          //defie_up(denops);
           break;
         default:
           actions.start(denops, validated_args.path);
